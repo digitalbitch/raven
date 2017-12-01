9db7584ee126a9d3efd1abd728d7e92bbea16bce
digitalBitch <ghosttk@protonmail.com>
2017-12-01 07:51:21 +0000

    initial

---
+++ Raven.Utils/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Utils
+{
+    public class Network
+    {
+        public void Network_IPs()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                {
+                    if (!ip.IsDnsEligible)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ raven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Utils;
+using static System.Net.NetworkInformation.NetworkInterface;
+
+namespace raven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var network = new Network();
+            network.Network_IPs();
+            Console.Read();
+        }
+    }
+}
