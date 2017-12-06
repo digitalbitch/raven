54f10a40a99875234b990eca18cac502724e0f09
digitalBitch <ghosttk@protonmail.com>
2017-12-06 17:01:04 +0000

    adding ping

---
+++ Raven.Utils/Network.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Utils
+{
+    public class Network
+    {
+        public List<UnicastIPAddressInformation> Network_IPs()
+        {
+            var results=new List<UnicastIPAddressInformation>();
+
+            foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                results.AddRange(ni.GetIPProperties().UnicastAddresses.Where(ip => !ip.IsDnsEligible).Where(ip => ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork));
+            }
+
+            return results;
+        }
+
+        public Dictionary<string,long> Ping_IPs(List<UnicastIPAddressInformation> iPs)
+        {
+            var results=new Dictionary<string, long>();
+            foreach (var unicastIpAddressInformation in iPs)
+            {
+                try
+                {
+                    var address = unicastIpAddressInformation.Address.MapToIPv4();
+                    Ping ping = new Ping();
+                    var reply = ping.Send(address.ToString());
+                    results.Add(address.ToString(), reply.RoundtripTime);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    e = null;
+                }
+              
+              
+            }
+            return results;
+        }
+    }
+}
--- raven/Program.cs
+++ raven/Program.cs
@@ -13,9 +13,29 @@ namespace raven
     {
         static void Main(string[] args)
         {
-            var network = new Network();
-            network.Network_IPs();
-            Console.Read();
+            try
+            {
+                var network = new Network();
+                var localIPs = network.Network_IPs();
+                Console.WriteLine("getting local address info");
+                foreach (var unicastIpAddressInformation in localIPs)
+                {
+                    Console.WriteLine(unicastIpAddressInformation.Address.ToString());
+                }
+                Console.WriteLine("pinging");
+                var pingResults = network.Ping_IPs(localIPs);
+                foreach (var pingResult in pingResults)
+                {
+                    Console.WriteLine(pingResult.Key, pingResult.Value);
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                e = null;
+            }
+           
         }
     }
 }
